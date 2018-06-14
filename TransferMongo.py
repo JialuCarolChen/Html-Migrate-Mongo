4da5933c9c3e9d7fd06be66e5eae59dd05e232b9
chenjialu <jche6589@uni.sydney.edu.au>
2018-06-14 14:03:49 +1000

    handle large file with GridFS

--- TransferMongo.py
+++ TransferMongo.py
@@ -1,7 +1,10 @@
 from pymongo import MongoClient
+from pymongo.errors import DocumentTooLarge
 from ConvertHtmlJson import ConvertHtmlJson
+from gridfs import  GridFS
 import bs4, requests
 import urllib
+import gridfs
 
 class TransferMongo(object):
 
@@ -23,13 +26,24 @@ class TransferMongo(object):
         json_docs = ConvertHtmlJson().convert_html2json(html_folder=html_folder)
         db = self.client[dbname]
         collection = db[colname]
+        # GridFS collection for large file
+        fs = GridFS(db)
         for jdoc in json_docs:
             # get file name to check whether the document exist
             fn = jdoc["file_name"]
             # if it doesn't exist
             if collection.find({'file_name': fn}).count() == 0:
-                collection.insert(jdoc)
-                print("Inserted document: "+fn)
+                try:
+                    collection.insert(jdoc)
+                    print("Inserted document: "+fn)
+                except DocumentTooLarge:
+                    if fs.find({"filename":fn}).count() == 0:
+                        html_content = jdoc["html_content"]
+                        html_content = str(html_content).encode("UTF-8")
+                        fs.put(html_content, filename = fn)
+                        print("Inserted document: " + fn)
+                    else:
+                        print(fn+" already exist!")
             else:
                 print(fn+" already exist!")
 
@@ -58,14 +72,32 @@ class TransferMongo(object):
                 f.write(html)
         return htmls, fns
 
+    def fetch_GridFS(self, dbname, output_html_folder):
+        db = self.client[dbname]
+        # GridFS collection for large file
+        fs = GridFS(db)
+
+        for fn in fs.list():
+            file_path = output_html_folder+'/'+fn
+            for fs_doc in fs.find({"filename":fn}):
+                fs_doc  = fs_doc.read()
+                with open(file_path, 'w') as f:
+                    f.write(fs_doc.decode('utf-8'))
 
 
-html_folder = "/Users/chenjialu/Desktop/Rassure/code/test_html_files"
+
+
+
+html_folder = "/Users/chenjialu/Desktop/Rassure/nltk/Html-Migrate-Mongo/test_html_files"
 output_html_folder = html_folder + '/output'
 
 tm = TransferMongo('localhost', 27017)
 tm.import_htmlfiles(html_folder=html_folder, dbname="rassure_nltk", colname="fare_html")
 htmls, fns = tm.fetch_htmlfiles(output_html_folder=output_html_folder, dbname="rassure_nltk", colname="fare_html")
+
+tm.fetch_GridFS(output_html_folder=output_html_folder, dbname="rassure_nltk")
+
+
 
 # check whether the string extract the html files match with the string extract from mongodb
 def assertion_check():
@@ -76,7 +108,13 @@ def assertion_check():
         assert inserted_html == str(htmls[i]), ("The string extracted from an inserted html file doesn't match with the html string reverted from mongodb")
 # do an assertion_check
 # Note: this will slow down the code!
-assertion_check()
+# assertion_check()
+
+
+
+
+
+
 
 
 
